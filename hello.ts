10ffd96c32be9eaec8e490932280e5e253636353
pisa-kun <pisa0401mst@gmail.com>
2024-08-30 13:56:15 +0900

    add settings

---
+++ hello.ts
@@ -0,0 +1,8 @@
+// unused-variable
+// const f = (a: number, b: number): number => a * 2;
+const f = (a: number): number => a * 2;
+
+//   console.log(f(3,4))
+// "prettier --write *.ts"の実行でスペースがなくなる
+console.log(f(3, 4));
+console.log(f(3, 2));
